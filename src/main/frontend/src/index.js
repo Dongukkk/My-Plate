169ae3c154cd2ac85bf556c69bdafe970830a4aa
Dongukkk <kdu0816@gmail.com>
2025-08-29 14:36:17 +0900

    리덕스 설정

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -6,8 +6,10 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
+  
 
 );
 
